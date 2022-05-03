ad7f83531b10a0b80c31125ed1e87f541e18f635
erikhuallpa <103543930+UABvladimir@users.noreply.github.com>
2022-05-03 03:47:05 -0400

    Se realizo la tarea t9 y t10 de HU-8

--- consultar_tramite.php
+++ consultar_tramite.php
@@ -1,42 +1,48 @@
 <?php session_start(); ?>
 <html>
-	<head>
-		<title>Formulario de Registro</title>
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-	</head>
-	<body>
+
+<head>
+	<title>Consultar Tramites</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
 	<?php include "php/menu1.php"; ?>
-<div class="container">
-<div class="row">
-<div class="col-md-10">
-		<h2><center>Consulta de tramite</center></h2>
-<br>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10">
+				<h2>
+					<center>Consulta de tramite</center>
+				</h2>
+				<br>
+				<form role="form" name="login" action="estado_tramite.php" method="post">
+					<center>
+						<table>
+							<tr>
+								<div class="form-group">
+									<td HEIGHT="50"> <label for="ci">Cedula de Identidad:</label> </td>
+									<td><input type="number" class="form-control" id="ci" name="ci" placeholder="Ingrese su C.I." required> </td>
 
+							</tr>
+							<tr>
+								<div class="form-group">
+									<td HEIGHT="50"><label for="fecha_nacimiento">Fecha de Nacimiento: </label> </td>
+									<td><input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" required> </td>
+								</div>
+							</tr>
+						</table>
+					</center>
+					<br>
+					<center>
+						<div class="form-group">
+							<input class="btn btn-primary" type="submit" value="Consultar">
+						</div>
+					</center>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- <script src="js/valida_login.js"></script> -->
+</body>
 
-		<form role="form" name="login" action="estado_tramite.php" method="post">
-
-		<center><table>
-		<tr>
-		  <div class="form-group">
-		  <td HEIGHT="50"> <label for="usuario">Cedula de Identidad:</label> </td>
-		  <td><input type="text" class="form-control" id="usuario" name="usuario" placeholder="Ingrese su C.I."> </td>
-		  </div>
-		</tr>
-
-		<tr>
-		  <div class="form-group">
-		  <td HEIGHT="50"><label for="password">Fecha de Nacimiento: </label> </td>
-		  <td><input type="date" class="form-control" id="password" name="password" placeholder="Contrase&ntilde;a"> </td>
-		  </div>
-		</tr>
-		</table></center>
-
-		<br>
-          <center> <button href="./estado_tramite.php">Consultar</button></center>
-		</form>
-</div>
-</div>
-</div>
-		<script src="js/valida_login.js"></script>
-	</body>
 </html>
--- estado_tramite.php
+++ estado_tramite.php
@@ -1,66 +1,108 @@
-<?php session_start(); ?>
+<?php
+session_start();
+
+$ci = $_POST['ci'];
+$fechaNacimiento = $_POST['fecha_nacimiento'];
+
+require_once("./php/conexion.php");
+
+$consultaTramite = "SELECT t.id_tramite, e.ci,e.nombres,e.paterno,e.materno,e.fecha_nacimiento,t.estado, t.fecha 
+FROM tramite t
+INNER JOIN egresado e ON e.id_egresado=t.id_egresado
+WHERE e.ci = $ci AND e.fecha_nacimiento = '$fechaNacimiento'";
+
+$respuesta = mysqli_query($con, $consultaTramite);
+$fila = mysqli_fetch_array($respuesta);
+
+?>
 <html>
-	<head>
-		<title>Formulario de Registro</title>
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-	</head>
-	<body>
-	<?php include "php/menu1.php"; ?><!-- mejora visual-->
-<div class="container">
-<div class="row">
-<div class="col-md-10">
-<center><h2>Estado de tramite</h2> </center>
-		
 
+<head>
+	<title>Formulario de Registro</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+</head>
 
+<body>
+	<?php include "php/menu1.php"; ?>
+	<!-- mejora visual-->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10">
+				<center>
+					<h2>Estado de tramite</h2>
+				</center>
 
-		<form role="form" name="registro" action="php/registro.php" method="post">
-		 		
-		
-		<center><table>
-		<div class="form-group">
-			<tr>	
-		    <td WIDTH="190" HEIGHT="50"><FONT COLOR="BLUE"><label for="ci">C.I.:</label></FONT></td>
-			<td  HEIGHT="50"><label for="ci">12345679</label></td>
-			</tr>
-			 </div>
-		 <tr> 
-		  	<div class="form-group">		  
-		    <td HEIGHT="50"><FONT COLOR="BLUE"><label for="nombres">Nombres:</label></FONT></td>
-			<td  HEIGHT="50"><label for="nombres">Jose Luis</label></td>			
-		     </div>
-		  </tr>
-		  <tr>
-		    <div class="form-group">		  
-		    <td  HEIGHT="50"><FONT COLOR="BLUE"><label for="paterno">Apellido Paterno:</label></FONT></td>
-			<td  HEIGHT="50"><label for="paterno">Cespedes</label></td>		
-		    </div>
-		  </tr>
-		 <tr>
-		   <div class="form-group">		  
-			<td  HEIGHT="50"> <FONT COLOR="BLUE"><label for="materno">Apellido Materno:</label></FONT></td>
-		    <td  HEIGHT="50"> <label for="materno">Caceres</label></td>						
-		  </div>
-		</tr>
-		<tr>
-		  <div class="form-group">		
-		    <td  HEIGHT="50"><FONT COLOR="BLUE"><label for="fecha_inicio">Fecha de Inicio:</label></FONT></td>
-			<td HEIGHT="50"><label for="correo">15/04/2022</label></td>					
-		  </div>
-		  </tr>
-		  <tr>
-		  <div class="form-group">		
-			<td  HEIGHT="50"><FONT COLOR="BLUE"><label for="estado">Estado:</label></FONT></td>
-			<td  HEIGHT="50"><FONT COLOR="RED"><label for="correo">EN CURSO</label></FONT></td>			
-		  </div>
-		  </tr>	
-			</table> </center>
-
-		  <center><button type="submit" class="btn btn-default" href="./loyout.php">Salir</button> </center>
-		</form>
+				<center>
+					<table>
+						<div class="form-group">
+							<tr>
+								<td WIDTH="190" HEIGHT="50">
+									<FONT COLOR="BLUE"><label for="ci">C.I.:</label></FONT>
+								</td>
+								<td HEIGHT="50"><label for="ci"><?= $fila['ci'] ?></label></td>
+							</tr>
+						</div>
+						<tr>
+							<div class="form-group">
+								<td HEIGHT="50">
+									<FONT COLOR="BLUE"><label for="nombres">Nombres:</label></FONT>
+								</td>
+								<td HEIGHT="50"><label for="nombres"><?= $fila['nombres'] ?></label></td>
+							</div>
+						</tr>
+						<tr>
+							<div class="form-group">
+								<td HEIGHT="50">
+									<FONT COLOR="BLUE"><label for="paterno">Apellido Paterno:</label></FONT>
+								</td>
+								<td HEIGHT="50"><label for="paterno"><?= $fila['paterno'] ?></label></td>
+							</div>
+						</tr>
+						<tr>
+							<div class="form-group">
+								<td HEIGHT="50">
+									<FONT COLOR="BLUE"><label for="materno">Apellido Materno:</label></FONT>
+								</td>
+								<td HEIGHT="50"> <label for="materno"><?= $fila['materno'] ?></label></td>
+							</div>
+						</tr>
+						<tr>
+							<div class="form-group">
+								<td HEIGHT="50">
+									<FONT COLOR="BLUE"><label for="fecha_inicio">Fecha de Inicio:</label></FONT>
+								</td>
+								<td HEIGHT="50"><label for="correo"><?= $fila['fecha'] ?></label></td>
+							</div>
+						</tr>
+						<tr>
+							<div class="form-group">
+								<td HEIGHT="50">
+									<FONT COLOR="BLUE"><label for="estado">Estado:</label></FONT>
+								</td>
+								<td HEIGHT="50">
+									<?php
+									$estado = "";
+									if ($fila['estado'] == '1') {
+										$estado = "En curso";
+									} else if ($fila['estado'] == '2') {
+										$estado = "En curso";
+									} else {
+										$estado = "Observado";
+									}
+									?>
+									<FONT COLOR="RED"><label for="correo"><?= $estado ?></label></FONT>
+								</td>
+							</div>
+						</tr>
+					</table>
+				</center>
+				<center>
+					<a class="btn btn-default" href='./consultar_tramite.php'>Salir</a>
+				</center>
+			</div>
 		</div>
-		</div>
-		</div>
+	</div>
+	<!-- valida tipo de datos -->
+	<!-- <script src="js/valida_registro.js"></script> -->
 
-		<script src="js/valida_registro.js"></script><!-- valida tipo de datos -->
 </html>
