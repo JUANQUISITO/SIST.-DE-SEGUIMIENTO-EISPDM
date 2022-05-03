e5262922382b9fc9fed3fd4b3b8e23a86d8d0d3a
erikhuallpa <103543930+UABvladimir@users.noreply.github.com>
2022-05-03 04:12:02 -0400

    Se completo las tareas faltantes.

--- php/registro.php
+++ php/registro.php
@@ -47,11 +47,30 @@ VALUES ('$ci','$nombres','$paterno','$materno','$correo ','$fecha_nacimiento','$
 
 //Ejecutar la consulta
 $respuesta = mysqli_query($con, $consulta);
-
+$ciUltimo = mysqli_insert_id($con);
+$mensaje = "";
 //Verificar la respuesta
 if ($respuesta) {
-	$mensaje =  "Se guardo correctamente";
+	//$mensaje =  "Se guardo correctamente";
+	$tipoConclucion = 1;
+	$tipoCIEncargado = 3242342;
+	$estado = 1;
+	$gestion = 1;
+	$fecha = date("Y-m-d");
+
+	$consulta2 = "INSERT INTO tramite (id_tipo, id_egresado, id_encargado, estado, id_gestion, fecha)
+	VALUES ('$tipoConclucion','$ciUltimo','$tipoCIEncargado','$estado','$gestion','$fecha');";
+	$respuesta = mysqli_query($con, $consulta2);
+	if ($respuesta) {
+	} else {
+		$mensaje = "Se guardo correctamente";
+	}
 } else {
 	$mensaje = "Error al guardar, intente de nuevo";
 }
 echo $mensaje;
+?>
+
+<div class="form-group">
+	<a class="btn btn-default" href='../index.php'>Regresar</a>
+</div>
