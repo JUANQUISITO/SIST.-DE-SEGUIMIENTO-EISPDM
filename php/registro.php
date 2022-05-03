b628d20b07638719fbd6dd1d05ee1f33782314c3
erikhuallpa <103543930+UABvladimir@users.noreply.github.com>
2022-05-03 03:15:31 -0400

    Se realizo el la pagina registro_conclucion, para el llamado de la carrera y gestion

--- php/registro.php
+++ php/registro.php
@@ -1,28 +1,57 @@
 <?php
-               //verifica si ya existe en la base de datos
-if(!empty($_POST)){
-	if(isset($_POST["ci"]) &&isset($_POST["nombres"]) &&isset($_POST["paterno"]) &&isset($_POST["materno"]) &&isset($_POST["fecha_nacimiento"]) &&isset($_POST["usuario"]) &&isset($_POST["correo"]) &&isset($_POST["password"]) &&isset($_POST["confirm_password"]) &&isset($_POST["item"])){
-		if($_POST["ci"]!=""&& $_POST["nombres"]!=""&&$_POST["paterno"]!=""&&$_POST["materno"]!=""&&$_POST["fecha_nacimiento"]!=""&&$_POST["usuario"]!=""&&$_POST["correo"]!=""&&$_POST["password"]==$_POST["confirm_password"]&&$_POST["item"]!=""){
-			include "conexion.php";
-			
-			$found=false;
-			$sql1= "select * from encargado where usuario=\"$_POST[usuario]\" or correo=\"$_POST[correo]\"";
-			$query = $con->query($sql1);
-			while ($r=$query->fetch_array()) {
-				$found=true;
-				break;
-			}
-			if($found){
-				print "<script>alert(\"Nombre de usuario o email ya estan registrados.\");window.location='../registro.php';</script>";
-			}
-			$sql = "insert into encargado(id_encargado,ci,nombres,paterno,materno,usuario,fecha_nacimiento,correo,contraseña,item) value (\"$_POST[ci]\",\"$_POST[ci]\",\"$_POST[nombres]\",\"$_POST[paterno]\",\"$_POST[materno]\"\"$_POST[fecha_nacimiento]\",\"$_POST[usuario]\",\"$_POST[correo]\",\"$_POST[password]\",\"$_POST[item]\",NOW())";
-			$query = $con->query($sql);
-			if($query!=null){
-				print "<script>alert(\"Registro exitoso. Proceda a logearse\");window.location='../login.php';</script>";
-			}
-		}
-	}
+//verifica si ya existe en la base de datos
+
+// if (!empty($_POST)) {
+// 	if (isset($_POST["ci"]) && isset($_POST["nombres"]) && isset($_POST["paterno"]) && isset($_POST["materno"]) && isset($_POST["fecha_nacimiento"]) && isset($_POST["correo"]) && isset($_POST["password"]) && isset($_POST["confirm_password"]) && isset($_POST["item"])) {
+// 		if ($_POST["ci"] != "" && $_POST["nombres"] != "" && $_POST["paterno"] != "" && $_POST["materno"] != "" && $_POST["fecha_nacimiento"] != "" &&  $_POST["correo"] != "" && $_POST["password"] == $_POST["confirm_password"] && $_POST["item"] != "") {
+// 			include "conexion.php";
+// 			$found = false;
+// 			$sql1 = "select * from encargado where usuario=\"$_POST[usuario]\" or correo=\"$_POST[correo]\"";
+// 			$query = $con->query($sql1);
+// 			while ($r = $query->fetch_array()) {
+// 				$found = true;
+// 				break;
+// 			}
+// 			if ($found) {
+// 				print "<script>alert(\"Nombre de usuario o email ya estan registrados.\");window.location='../registro.php';</script>";
+// 			}
+// 			$sql = "insert into encargado(id_encargado,ci,nombres,paterno,materno,usuario,fecha_nacimiento,correo,contraseña,item) value (\"$_POST[ci]\",\"$_POST[ci]\",\"$_POST[nombres]\",\"$_POST[paterno]\",\"$_POST[materno]\"\"$_POST[fecha_nacimiento]\",\"$_POST[usuario]\",\"$_POST[correo]\",\"$_POST[password]\",\"$_POST[item]\",NOW())";
+// 			$query = $con->query($sql);
+// 			if ($query != null) {
+// 				print "<script>alert(\"Registro exitoso. Proceda a logearse\");window.location='../login.php';</script>";
+// 			}
+// 		}
+// 	}
+// }
+
+// require_once("./php/conexion.php");
+require_once("../php/conexion.php");
+
+$ci = $_POST["ci"];
+$nombres = $_POST["nombres"];
+$paterno = $_POST["paterno"];
+$materno = $_POST["materno"];
+$correo = $_POST["correo"];
+$fecha_nacimiento = $_POST["fecha_nacimiento"];
+$celular = $_POST["celular"];
+$carrera = $_POST["carrera"];
+$gestion = $_POST["gestion"];
+
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
+
+
+$consulta = "INSERT INTO egresado(ci, nombres, paterno, materno, email, fecha_nacimiento, celular, id_carrera) 
+VALUES ('$ci','$nombres','$paterno','$materno','$correo ','$fecha_nacimiento','$celular','$carrera');";
+
+//Ejecutar la consulta
+$respuesta = mysqli_query($con, $consulta);
+
+//Verificar la respuesta
+if ($respuesta) {
+	$mensaje =  "Se guardo correctamente";
+} else {
+	$mensaje = "Error al guardar, intente de nuevo";
 }
-
-
-?>
+echo $mensaje;
--- registro_conclusion.php
+++ registro_conclusion.php
@@ -1,109 +1,133 @@
-<?php session_start(); ?>
+<?php
+session_start();
+require_once("./php/conexion.php");
+
+$consultaCarrera = "SELECT * FROM carrera";
+$resultadoCarrera = mysqli_query($con, $consultaCarrera);
+
+$consultaGestion = "SELECT * FROM gestion";
+$resultadoGestion = mysqli_query($con, $consultaGestion);
+
+//var_dump($resultadoCarrera);
+
+?>
 <html>
-	<head>
-		<title>Formulario de Registro</title>
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-	</head>
-	<body>
-	<?php include "php/menu2.php"; ?><!-- mejora visual-->
-<div class="container">
-<div class="row">
-<div class="col-md-10">
-<center><h2>Datos de registro</h2> </center>
-		
+
+<head>
+	<title>Formulario de Registro</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+	<?php include "php/menu2.php"; ?>
+	<!-- mejora visual-->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10">
+				<center>
+					<h2>Datos de registro</h2>
+				</center>
+				<form role="form" name="registro" action="php/registro.php" method="post">
+					<center>
+						<table>
+							<tr>
+								<div class="form-group">
+									<td WIDTH="190" HEIGHT="50"><label for="ci">Cedula de identidad:</label></td>
+									<td> <input type="text" class="form-control" id="ci" name="ci" placeholder="Cedula de identidad" required></td>
+								</div>
+							</tr>
+							<tr>
+								<div class="form-group">
+
+									<td WIDTH="190" HEIGHT="50"><label for="nombres">Nombres:</label></td>
+									<td><input type="int" class="form-control" id="nombres" name="nombres" placeholder="Nombres" required></td>
+
+								</div>
+							</tr>
+							<tr>
+								<div class="form-group">
+									<td WIDTH="190" HEIGHT="50"><label for="paterno">Apellido Paterno:</label></td>
+									<td><input type="text" class="form-control" id="paterno" name="paterno" placeholder="Apellido Paterno" required></td>
+								</div>
+							</tr>
+							<tr>
+								<div class="form-group">
+
+									<td WIDTH="190" HEIGHT="50"> <label for="materno">Apellido Materno:</label></td>
+									<td><input type="text" class="form-control" id="materno" name="materno" placeholder="Apellido Materno" required></td>
+
+								</div>
+							</tr>
+							<tr>
+								<div class="form-group">
+
+									<td WIDTH="190" HEIGHT="50"><label for="correo">Correo Electronico:</label></td>
+									<td> <input type="email" class="form-control" id="correo" name="correo" placeholder="Correo Electronico" required></td>
+
+								</div>
+							</tr>
+							<tr>
+								<div class="form-group">
+
+									<td WIDTH="190" HEIGHT="50"><label for="fecha_nacimiento">Fecha de Nacimiento:</label></td>
+									<td><input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" placeholder="Fecha de Nacimiento"></td>
+
+								</div>
+							</tr>
+							<tr>
+								<div class="form-group">
+
+									<td WIDTH="190" HEIGHT="50"><label for="item">Celular:</label></td>
+									<td><input type="text" class="form-control" id="Celular" name="celular" placeholder="ingrese su numero de celular" required></td>
+
+								</div>
+							</tr>
+							<!--Aqui cargar carreras de la base de datos-->
+							<tr>
+								<div class="form-group">
+									<td WIDTH="190" HEIGHT="50"> <label for="item">Carrera:</label></td>
+									<td><select class="form-control " name="carrera" id="id_carrera">
+											<option value="">Seleccione una Carrera</option>
+
+											<?php while ($fila = mysqli_fetch_array($resultadoCarrera)) { ?>
+												<option value="<?= $fila['id_carrera'] ?>">
+													<?= $fila['nombre'] ?>
+												</option>
+											<?php } ?>
+
+										</select> </td>
+								</div>
+							<tr>
 
 
 
-		<form role="form" name="registro" action="php/registro.php" method="post">
-		 		
-		
-		<center><table>
-		<tr>
-		    <div class="form-group">				
-		    <td WIDTH="190" HEIGHT="50"><label for="ci">Cedula de identidad:</label></td>
-			<td> <input type="text" class="form-control" id="ci" name="ci" placeholder="Cedula de identidad"></td>			
-			 </div>
-		</tr>
-		<tr> 
-		  <div class="form-group">		  
-			 
-		    <td WIDTH="190" HEIGHT="50"><label for="nombres">Nombres:</label></td>
-		    <td><input type="int" class="form-control" id="nombres" name="nombres" placeholder="Nombres"></td>
-						
-		  </div>
-		  </tr>
-		  <tr>
-		  <div class="form-group">
-		    <td WIDTH="190" HEIGHT="50"><label for="paterno">Apellido Paterno:</label></td>
-		    <td><input type="text" class="form-control" id="paterno" name="paterno" placeholder="Apellido Paterno"></td>						
-		  </div>
-		  </tr>
-		  <tr>
-		  <div class="form-group">		  
-			
-			<td WIDTH="190" HEIGHT="50"> <label for="materno">Apellido Materno:</label></td>
-		    <td><input type="text" class="form-control" id="materno" name="materno" placeholder="Apellido Materno"></td>
-						
-		  </div>
-		  </tr>
-		  <tr>
-		  <div class="form-group">		 
-			
-		    <td WIDTH="190" HEIGHT="50"><label for="correo">Correo Electronico:</label></td>
-			<td> <input type="email" class="form-control" id="correo" name="correo" placeholder="Correo Electronico"></td>
-				
-		  </div>
-		  </tr>	
-		  <tr>
-		  <div class="form-group">		  
-			
-		    <td WIDTH="190" HEIGHT="50"><label for="fecha_nacimiento">Fecha de Nacimiento:</label></td>
-		    <td><input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" placeholder="Fecha de Nacimiento"></td>
-						
-		  </div>
-		  </tr>
-		  <tr>		  
-		  <div class="form-group">		 
-			
-		    <td WIDTH="190" HEIGHT="50"><label for="item">Celular:</label></td>
-		    <td><input type="text" class="form-control" id="Celular" name="Celular" placeholder="ingrese su numero de celular"></td>
-						
-		  </div>
-		  </tr>
-			<!--Aqui cargar carreras de la base de datos-->
-		<tr>
-		  	<div class="form-group">		
-			<td WIDTH="190" HEIGHT="50"> <label for="item">Carrera:</label></td>
-			<td><select name="ciLocationUser" class="form-control" id="ciLocationUser">
-			<option value="sc">--Seleccione la carrera---</option>  
-			<option value="ii">Informatica Industrial</option>  
-			<option value="qi">Quimica Industrial</option> 
-			<option value="ei">Electriciad Industrial</option> 
-			<option value="ma">Mecanica Automotriz</option> 				
-			</select>	</td>					
-		  	</div>
-		  <tr>
+								<!--Aqui cargar gestion de la base de datos-->
+							<tr>
+								<div class="form-group">
+									<td WIDTH="190" HEIGHT="50"> <label for="item">Gestión:</label></td>
+									<td><select class="form-control " name="gestion" id="id_gestion">
+											<option value="">Seleccione una Carrera</option>
 
-			<!--Aqui cargar gestion de la base de datos-->
-			<tr>
-		    <div class="form-group">  			
-			<td WIDTH="190" HEIGHT="50"> <label for="item">Gestión:</label></td>
-		    <td><select name="ciLocationUser" class="form-control" id="ciLocationUser"> 
-			<option value="sc">--Seleccione la Gestión---</option>  
-			<option value="ii">I/2021</option>  
-			<option value="qi">II/2021</option> 
-			<option value="ei">I/2022</option> 								
-			</select>	</td> 		
-		    </div>
-			<tr>
+											<?php while ($fila = mysqli_fetch_array($resultadoGestion)) { ?>
+												<option value="<?= $fila['id_gestion'] ?>">
+													<?= $fila['detalle'] ?>
+												</option>
+											<?php } ?>
 
-</table> </center>
-		  <center><button type="submit" class="btn btn-default">Registrar</button> </center>
-		</form>
+										</select> </td>
+								</div>
+							<tr>
+
+						</table>
+					</center>
+					<center><button type="submit" class="btn btn-default">Registrar</button> </center>
+				</form>
+			</div>
 		</div>
-		</div>
-		</div>
+	</div>
 
-		<script src="js/valida_registro.js"></script><!-- valida tipo de datos -->
-	</body>
+	<!-- valida tipo de datos -->
+	<!-- <script src="js/valida_registro.js"></script> -->
+</body>
+
 </html>
